07538651a0d54fbae8ffe148bfc52557fb918cde
githubgib <gibson.ikoro@gmail.com>
2024-03-09 23:31:38 +0000

    Update goodcode.py

--- goodcode.py
+++ goodcode.py
@@ -1,7 +1,8 @@
 def add_numbers(a, b):
+    """Add two numbers."""
     return a + b
 
 
 # Corrected the spacing around these comments to adhere to PEP 8
 print(add_numbers(3, 4))  # Add numbers 3 and 4
-print(add_numbers(5,6))  # Add numbers 5 and 6
+print(add_numbers(5, 6))  # Add numbers 5 and 6
